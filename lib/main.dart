d54631dece2c52a20a7b8635633bc0ed2ae06499
sanika2001 <porayilsanika@gmail.com>
2022-07-23 13:01:18 +0530

    Reg Screen modification

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bibliotheca/Screens/LoginScreen.dart';
 import 'package:bibliotheca/Screens/OnboardingScreen.dart';
+import 'package:bibliotheca/Screens/RegistrationScreen.dart';
 import 'package:bibliotheca/Screens/SplashScreen.dart';
 import 'package:bibliotheca/Screens/bg.dart';
 import 'package:flutter/material.dart';
@@ -16,11 +17,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: bg.id,
+        initialRoute: RegistrationScreen.id,
         routes: {
           SplashScreen.id: (context) => SplashScreen(),
           OnboardingScreen.id: (context) => OnboardingScreen(),
           LoginScreen.id: (context) => LoginScreen(),
+          RegistrationScreen.id: (context) => RegistrationScreen(),
           bg.id: (context) => bg(),
         });
   }
