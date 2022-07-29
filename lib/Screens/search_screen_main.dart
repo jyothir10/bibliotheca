ff6a2c7d5c6f174738577a1d0a481a94eedbf9a2
Parvathi Rajpal <parvathirajpal@gmail.com>
2022-07-29 22:42:44 +0530

    Completed Main Search Screen

---
+++ lib/Screens/search_screen_main.dart
@@ -0,0 +1,91 @@
+import 'package:bibliotheca/Components/WhiteButton.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../Components/BlueButton.dart';
+
+class SearchScreenMain extends StatefulWidget {
+  static const String id = '/search1';
+  const SearchScreenMain({Key? key}) : super(key: key);
+
+  @override
+  State<SearchScreenMain> createState() => _SearchScreenMainState();
+}
+
+class _SearchScreenMainState extends State<SearchScreenMain> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(children: <Widget>[
+        Positioned(
+          top: 0,
+          left: 49,
+          child: Container(
+              width: 365,
+              height: 204,
+              child: SvgPicture.asset("images/Group 108.svg")),
+        ),
+        Positioned(
+          left: 12,
+          top: 48,
+          child: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.arrow_back_rounded,
+              size: 30,
+              color: Color(0xFF545AD8),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 180,
+          left: 30,
+          child: SafeArea(
+            child: Column(
+              children: [
+                SizedBox(
+                  child: Image.asset("images/search.gif"),
+                  width: 200,
+                  height: 200,
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 15),
+                  child: const Text(
+                    'Search Books By: ',
+                    style: TextStyle(
+                      color: Colors.black45,
+                      fontSize: 22,
+                      fontFamily: 'Montserrat',
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 60,bottom: 20),
+                  child: WhiteButton(
+                    text: 'Book Name',
+                    width: 170,
+                    onTap: () {},
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin:
+                            EdgeInsets.only(top: 15, right: 20,bottom: 15),
+                        child: BlueButton(
+                            text: 'Author', width: 140, onTap: () {}, Colour: 0xff5C6BC0,),
+                      ),
+                      BlueButton(text: 'Department', width: 180, onTap: () {}, Colour: 0xff545ad8,),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ]),
+    );
+  }
+}
