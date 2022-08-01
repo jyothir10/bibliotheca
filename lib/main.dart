8b31d6ecc3b7be3aa648765736c05d712fadd240
sanika2001 <porayilsanika@gmail.com>
2022-08-01 17:54:06 +0530

    Review book screen-1

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
               ),
         ),
         debugShowCheckedModeBanner: false,
-        initialRoute: LoginScreen.id,
+        initialRoute: SplashScreen.id,
         routes: {
           SplashScreen.id: (context) => SplashScreen(),
           OnboardingScreen.id: (context) => OnboardingScreen(),
